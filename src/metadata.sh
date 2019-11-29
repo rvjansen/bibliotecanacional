2566a4025fb4aff67920d35d92a53c36cf993ccb
René Vincent Jansen <rvjansen@xs4all.nl>
2019-11-29 03:56:19 -0400

    progress

--- src/metadata.sh
+++ src/metadata.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env zsh
 
+cp ~/bibliotecanacional/src/getEssoMonth.class .
 # upload a directory of .zip files
 pipe "literal ls -1 | command | locate /.zip/ | specs /ia upload/ 1 1-27 11 /--metadata=\"mediatype:texts\" --metadata=\"collection:lagocollection\" --metadata=\"collection:arubanewspapers\" --metadata=\"collection:bibliotecanacionalaruba\"/ 40 1-* 197 | > upload.sh"
 
 #add eng as a language
-pipe "literal ls -1 | command | locate /.zip/ | specs /ia metadata/ 1 1-27 14 /--modify=\"language:eng\"/ 45 | >update_language"
+pipe "literal ls -1 | command | locate /.zip/ | specs /ia metadata/ 1 1-27 14 /--modify=\"language:eng\"/ 45 | >update_language.sh"
 
 #append Papiamento as a language
 pipe "literal ls -1 | command | locate /.zip/ | specs /ia metadata/ 1 1-27 14 /--append-list=\"language:Papiamento\"/ 45 | > append_language.sh"
@@ -28,7 +30,11 @@ pipe "literal ls -1 | command | locate /.zip/ | specs /ia metadata/ 1 1-27 14 /-
 #description
 pipe "literal ls -1 | command | locate /.zip/ | specs /ia metadata/ 1 1-27 13 /--modify=\"description:Aruba Esso News, A bi-weekly publication by Lago Oil & Transport Co. Ltd, San Nicolas, Aruba, in English and Papiamento. Digitized and made available online by Biblioteca Nacional Aruba.\"/ 41 | > update_description.sh"
 
+#title
+java org.netrexx.njpipes.pipes.runner "literal ls -1 | command | locate /.zip/ | specs 1-27 1 | getEssoMonth | >title.sh"
+
 #delete if we need to start over
 pipe "literal ls -1 | command | locate /.zip/ | specs /ia delete/ 1 1-27 12 /--all -H x-archive-keep-old-version:0/ 40 | >delete.sh"
 
 
+ 
