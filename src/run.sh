fce7bd97bc23df5479602dd80f4f28217bed6dbc
Rene Vincent Jansen <rvjansen@xs4all.nl>
2020-08-22 00:30:02 +0200

    add completion for rename arubaanse

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,9 @@
 rm *.zip
 cp ~/apps/bibliotecanacional/src/ArubaanseCourant.class .
 cp ~/apps/bibliotecanacional/src/IncrementalUpload*.class .
-source rename.sh
+cp ~/apps/bibliotecanacional/src/Complete.class .
+java Complete >renam2.sh
+source renam2.sh
 java ArubaanseCourant
 java IncrementalUpload
 source upload.sh
